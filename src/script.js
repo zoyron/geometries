5e8958235fdc06ce4a82d9c561cdce2a351e81ee
zoyron <sgrarora3@gmail.com>
2024-07-19 15:16:50 +0530

    created the convex geometry of a dodecahedron

--- src/script.js
+++ src/script.js
@@ -30,34 +30,74 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
  * camera
  */
 camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 1000);
-camera.position.set(0, 3, 3);
+camera.position.set(12, 25, 20);
 scene.add(camera);
+
+/**
+ * lights
+ */
+const light = new THREE.PointLight(0xffffff, 3, 0, 0);
+camera.add(light);
 
 /**
  * controlls - orbital controls
  */
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
-
+controls.minDistance = 20;
+controls.maxDistance = 50;
+controls.maxPolarAngle = Math.PI / 2;
 /**
  * texture
  */
 const loader = new THREE.TextureLoader();
 const texture = loader.load(disc);
+texture.colorSpace = THREE.SRGBColorSpace;
 
 /**
  * dodecahedron
  */
 
-const dodecahedronGeometry = new THREE.DodecahedronGeometry(2, 1);
-const dodecahedronMaterial = new THREE.MeshBasicMaterial({
-  color: "red",
-  wireframe: true,
+let dodecahedronGeometry = new THREE.DodecahedronGeometry(10);
+dodecahedronGeometry.deleteAttribute("normal");
+dodecahedronGeometry.deleteAttribute("uv");
+dodecahedronGeometry = BufferGeometryUtils.mergeVertices(dodecahedronGeometry);
+
+const positions = dodecahedronGeometry.getAttribute("position");
+const vertices = [];
+for (let i = 0; i < positions.count; i++) {
+  const vertex = new THREE.Vector3();
+  vertex.fromBufferAttribute(positions, i);
+  vertices.push(vertex);
+}
+
+/**
+ * points material and stuff
+ */
+const pointsMaterial = new THREE.PointsMaterial({
+  color: 0x0080ff,
+  map: texture,
+  size: 1,
+  alphaTest: 0.5,
 });
-const mesh = new THREE.Mesh(dodecahedronGeometry, dodecahedronMaterial);
+
+const pointsGeometry = new THREE.BufferGeometry().setFromPoints(vertices);
+const points = new THREE.Points(pointsGeometry, pointsMaterial);
+scene.add(points);
+
+/**
+ * adding the convex hull
+ */
+const convexGeometry = new ConvexGeometry(vertices);
+const convexMaterial = new THREE.MeshLambertMaterial({
+  color: 0xff0000,
+  opacity: 0.25,
+  side: THREE.DoubleSide,
+  transparent: true,
+});
+const mesh = new THREE.Mesh(convexGeometry, convexMaterial);
 scene.add(mesh);
 camera.lookAt(mesh.position);
-
 /**
  * resize window
  */
@@ -80,7 +120,8 @@ window.addEventListener("resize", () => {
  * animation
  */
 const animate = () => {
-  // mesh.rotation.y += 0.025;
+  mesh.rotation.y += 0.005;
+  points.rotation.y += 0.005;
 
   renderer.render(scene, camera);
   window.requestAnimationFrame(animate);
